5d5c555b37f2edbef49f6039d7358f74994ad174
Devin Ambron <devin@ambron.net>
2024-09-15 10:48:56 -0500

    Fixed OpenAI import issue

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 import re
 import random
-from openai import OpenAI
+import openai
 
-client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+client = openai.OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 BOLD = '\033[1m'
 RED = '\033[31m'
